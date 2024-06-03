1dffd842307bcf47ae8ee91f2642815db6a38250
Sven Heis <svenheis@students.ibaw.ch>
2024-06-03 12:06:54 +0200

    a

--- src/pages/Main/Erfassen.jsx
+++ src/pages/Main/Erfassen.jsx
@@ -34,7 +34,7 @@ const Erfassen = () => {
     // Alle Angaben trimmen
     const Titel = inputs.titel.trim();
     const Typ = inputs.typ.trim();
-    const Betrag = inputs.betrag.toFixed(2);
+    const Betrag = inputs.betrag.trim();
     // Falls leere oder falsche Angaben kommen wird eine Meldung ausgegeben
     if (!Titel) {
       toast.error("Bitte einen Titel eingeben");
@@ -46,6 +46,10 @@ const Erfassen = () => {
     }
     if (!Betrag) {
       toast.error("Bitte einen Betrag eingeben");
+      return;
+    }
+    if (!/^\d*(\.\d[05]?)?$/.match(Betrag)) {
+      toast.error("Einen gültigen Betrag eingeben (auf 5 Rappen runden)");
       return;
     }
     try {
